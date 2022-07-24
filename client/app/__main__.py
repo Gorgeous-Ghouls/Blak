fb93fd2865228124a425da242a8136d6fedbffe3
p0lygun <solankivibhakar82@gmail.com>
2022-07-24 17:12:17 +0530

    feat: set WM_CLASS to Blak

--- client/app/__main__.py
+++ client/app/__main__.py
@@ -1,8 +1,10 @@
 import asyncio
-
-from .lib.kivy_manager import ClientUI
+import os
 
 if __name__ == "__main__":
+    os.environ["SDL_VIDEO_X11_WMCLASS"] = "Blak"
+    from .lib.kivy_manager import ClientUI
+
     client = ClientUI()
     try:
         asyncio.run(client.app_func())
