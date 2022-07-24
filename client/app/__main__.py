938548e6e1239824375404480d5aca641addffab
p0lygun <solankivibhakar82@gmail.com>
2022-07-24 16:55:15 +0530

    feat: exit gracefully when not able to connet to server

--- client/app/__main__.py
+++ client/app/__main__.py
@@ -4,4 +4,7 @@ from .lib.kivy_manager import ClientUI
 
 if __name__ == "__main__":
     client = ClientUI()
-    asyncio.run(client.app_func())
+    try:
+        asyncio.run(client.app_func())
+    except asyncio.exceptions.CancelledError:
+        pass
