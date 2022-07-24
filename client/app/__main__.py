8b9b6cbff60ec750d2e8e9041f134810975e3199
p0lygun <solankivibhakar82@gmail.com>
2022-07-24 19:33:13 +0530

    feat: add command line script to run app now use command poetry run blak to run client

--- client/app/__main__.py
+++ client/app/__main__.py
@@ -1,8 +1,12 @@
 import asyncio
 import os
 
-if __name__ == "__main__":
-    os.environ["SDL_VIDEO_X11_WMCLASS"] = "Blak"
+
+def main():
+    """A runner function that serves as an entry point for command scripts"""
+    os.environ[
+        "SDL_VIDEO_X11_WMCLASS"
+    ] = "Blak"  # required when using a tiling manager or WM
     from .lib.kivy_manager import ClientUI
 
     client = ClientUI()
@@ -10,3 +14,7 @@ if __name__ == "__main__":
         asyncio.run(client.app_func())
     except asyncio.exceptions.CancelledError:
         pass
+
+
+if __name__ == "__main__":
+    main()
