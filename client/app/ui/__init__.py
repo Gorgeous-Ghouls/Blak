c0af40fdd966e4bc6fcd0f4438a43fcdc02240c3
Vibhakar Solanki <solankivibhakar82@gmail.com>
2022-07-26 19:05:44 +0530

    feat: add label that shows connection status (#31)
    * feat: move TitleBar to ui module
    
    * feat: add label that shows connection status; closes #29
    
    * feat: add ability to logout; closes #33

--- client/app/ui/__init__.py
+++ client/app/ui/__init__.py
@@ -1,5 +1,11 @@
+from kivy.uix.screenmanager import ScreenManager
+from kivy.utils import get_color_from_hex
 from kivymd.app import MDApp
+from kivymd.uix.button import BaseButton
 from kivymd.uix.card import MDCard
+from kivymd.uix.floatlayout import MDFloatLayout
+
+from ..utils import Colors
 
 
 class ChatItem(MDCard):
@@ -25,6 +31,43 @@ class ChatItem(MDCard):
     def on_touch_up(self, touch):
         """Event Fired everytime mouse is released to tap is released."""
         if self.collide_point(*touch.pos):
-            MDApp.get_running_app().root.ids[
-                "screen_manager"
-            ].current = self.custom_id  # switch screen to the chat
+            screen_manager: ScreenManager
+            screen_manager = MDApp.get_running_app().root.ids["chats_screen_manager"]
+            if screen_manager.has_screen(self.custom_id):
+                screen_manager.current_screen = self.custom_id
+
+            # switch screen to the chat
+
+
+class TitleBar(MDFloatLayout):
+    """Custom TitleBar for the app"""
+
+    md_bg_color = get_color_from_hex(Colors.accent_bg.value)
+    button_bg = get_color_from_hex(Colors.primary_bg.value)
+    button_size = "15sp"
+
+    def __init__(self, **kwargs):
+        super(TitleBar, self).__init__(**kwargs)
+        from ..lib.kivy_manager import ClientUI
+
+        self.app: ClientUI = MDApp.get_running_app()
+
+    def handle_buttons(self, instance: BaseButton):
+        """Callback Function for all buttons in titlebar
+
+        :param instance a button object that is a subclass of kivymd.uix.button.BaseButton
+
+        A button object was chosen instead of say a string, so that later on the caller itself can be edited
+        """
+        match_string = None
+        if hasattr(instance, "icon"):
+            match_string = instance.icon
+
+        if match_string:
+            match match_string:
+                case "close":
+                    self.app.stop()
+                case "window-minimize":
+                    self.app.root_window.minimize()
+                case "window-maximize":
+                    self.app.root_window.maximize()
