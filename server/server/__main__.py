3fce823b5debacafbc0be6d181869f09867f6742
Kanishq <76529491+StoneSteel27@users.noreply.github.com>
2022-07-27 23:14:51 +0530

    feat: add fastapi backend 🥳 (#39)
    * feat: Started development of backend v0.0.1
    
    * feat:Database implementation (#14)
    
    * feat: Database implemented | server v0.0.2
    
    * fix: changing port 5000 -> 8000
    
    * feat: Added code for Connection Manager and Message Manager
    
    * #25 and #26 (#27)
    
    * perf: Database I/O made faster | issue (#25)
    
    * perf: Introduces User class for performance(#26)
    
    * feat: Complete Backend (#35)
    
    * perf: Database I/O made faster | issue (#25)
    
    * perf: Introduces User class for performance(#26)
    
    * feat: closes #15,#20,#21,#25,#26
    
    * fix: Update managers type hinting
    
    Co-authored-by: Vibhakar Solanki <solankivibhakar82@gmail.com>
    
    * fix: Update user.py
    
    Co-authored-by: Vibhakar Solanki <solankivibhakar82@gmail.com>
    
    * refactor: Refactoring server
    
    * feat: remove circular imports; bump package versions
    
    * refactor: fixed type hinting & return type issues
    
    * chore: fix typo of  receiver_id
    
    * feat: add function to ensure runtime files exist
    
    * chore: set corect path in gitignore for users,rooms,json
    
    * fix: managers.py filenotfound error resolved
    
    * chore: set corect path in gitignore for users,rooms,json
    
    * chore: remove unused import
    
    * style: fix formatting with black
    
    * fix: Database initialization
    
    * feat: rebase
    
    * style: typehint class vars
    
    * feat: add error handling
    
    * feat: add error handling; fix multiple typo; add logging
    
    * feat: add logging_level env var
    
    Co-authored-by: Vibhakar Solanki <solankivibhakar82@gmail.com>
    
    Co-authored-by: Aakarsh Jain <jainaakarsh09@gmail.com>
    Co-authored-by: Vibhakar Solanki <solankivibhakar82@gmail.com>

---
+++ server/server/__main__.py
@@ -0,0 +1,30 @@
+import os
+import pathlib
+import sys
+
+import uvicorn
+from dotenv import load_dotenv
+from loguru import logger
+
+load_dotenv()
+
+
+def ensure_files():
+    """Ensurers that runtime files exists."""
+    app_dir = pathlib.Path(__file__).parent
+    (app_dir / "users.json").touch(exist_ok=True)
+    (app_dir / "rooms.json").touch(exist_ok=True)
+
+
+def main():
+    """Main run entrypoint for backend"""
+    logging_level = os.getenv("logging_level", "INFO")
+    logger.remove()
+    logger.add(sys.stderr, level=logging_level)
+
+    ensure_files()
+    uvicorn.run("server.app:app", port=8000, log_level=logging_level.lower())
+
+
+if __name__ == "__main__":
+    main()
---
+++ server/server/app.py
@@ -0,0 +1,22 @@
+"""This is where the main backend app will go into"""
+
+from fastapi import FastAPI, WebSocket
+
+from .managers import ConnectionManager, DbManager
+
+app = FastAPI(debug=True)
+
+db = DbManager("server/users.json", "server/rooms.json")
+connections = ConnectionManager(db)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    """Websocket entrypoint"""
+    await connections.create_session(websocket)
+
+
+@app.on_event("shutdown")
+async def close_db():
+    """Saves the database"""
+    db.save()
