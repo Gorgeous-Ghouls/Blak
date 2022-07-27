7f9d5d3104c2eb1eca27e2ec66e808546eeed51c
p0lygun <solankivibhakar82@gmail.com>
2022-07-27 22:30:22 +0530

    feat: add logging_level env var

--- server/server/__main__.py
+++ server/server/__main__.py
@@ -1,6 +1,12 @@
+import os
 import pathlib
+import sys
 
 import uvicorn
+from dotenv import load_dotenv
+from loguru import logger
+
+load_dotenv()
 
 
 def ensure_files():
@@ -12,8 +18,12 @@ def ensure_files():
 
 def main():
     """Main run entrypoint for backend"""
+    logging_level = os.getenv("logging_level", "INFO")
+    logger.remove()
+    logger.add(sys.stderr, level=logging_level)
+
     ensure_files()
-    uvicorn.run("server.app:app", port=8000, log_level="debug")
+    uvicorn.run("server.app:app", port=8000, log_level=logging_level.lower())
 
 
 if __name__ == "__main__":
